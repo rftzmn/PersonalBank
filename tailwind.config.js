9498c419719ddcdaa97632e8ceca1696a0a83ba9
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2023-10-22 20:41:49 +0600

    tailwind elements installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['*'],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 }
 
